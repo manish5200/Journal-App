56b325de4e6fe99827feeaa14a8f56c62dd3714b
manish5200 <manishksingh5200gmail.com>
2025-08-27 22:11:15 +0530

    Adds jwt

--- src/main/java/net/manifest/journalApp/controller/PublicController.java
+++ src/main/java/net/manifest/journalApp/controller/PublicController.java
@@ -1,22 +1,35 @@
 package net.manifest.journalApp.controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import net.manifest.journalApp.entity.User;
+import net.manifest.journalApp.services.UserDetailsServiceImpl;
 import net.manifest.journalApp.services.UserService;
+import net.manifest.journalApp.utils.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/public")
 public class PublicController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+    @Autowired
+    private JwtUtils jwtUtils;
 
     @PostMapping("/signup")
-    public ResponseEntity<String>userEntry(@RequestBody User newUser) {
+    public ResponseEntity<String>signup(@RequestBody User newUser) {
 
 //        try{
 //            if(userService.findByUserName(newUser.getUserName()) != null){
@@ -29,6 +42,27 @@ public class PublicController {
 //        }
         userService.saveNewUser(newUser);
         return new ResponseEntity<>("Registration Successful.", HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String>login(@RequestBody User user) {
+         try{
+             authenticationManager.authenticate(
+                     new UsernamePasswordAuthenticationToken(
+                             user.getUserName(),
+                             user.getPassword())
+             );
+
+             UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUserName());
+             String jwt = jwtUtils.generateToken(userDetails.getUsername());
+             return new ResponseEntity<>("Login Successful: "
+                     + jwt,
+                     HttpStatus.OK);
+
+         }catch (Exception e){
+             log.error("Exception occurred while creating AuthenticationToken: "+"Login Method",e);
+             return new ResponseEntity<>("Incorrect username or password.",HttpStatus.BAD_REQUEST);
+         }
     }
 
     @GetMapping("/health-check")
---
+++ src/main/java/net/manifest/journalApp/filter/JwtFilter.java
@@ -0,0 +1,55 @@
+package net.manifest.journalApp.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import net.manifest.journalApp.utils.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        String token = null;
+        String username = null;
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            token = authHeader.substring(7);
+            username = jwtUtils.extractUsername(token);
+        }
+
+        if(username != null  && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            Boolean isValidToken = jwtUtils.validateToken(token);
+            if(Boolean.TRUE.equals(isValidToken)){
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities());
+                auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        }
+       // response.addHeader("admin","Manish");
+        filterChain.doFilter(request,response);
+    }
+}
